8902f3ea8416e15321c90bc5ca51b44d4dcac89d
Phuc Party <phuc.party@gmail.com>
2023-03-10 18:08:17 +0700

    gitsigns change current_line_blame_formatter

--- lua/user/gitsigns.lua
+++ lua/user/gitsigns.lua
@@ -17,11 +17,13 @@ gitsigns.setup {
     follow_files = true,
   },
   attach_to_untracked = true,
+  current_line_blame = true,
   current_line_blame_opts = {
     virt_text = true,
     virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
     delay = 1000,
   },
+  current_line_blame_formatter = '<author> - <author_time:%H:%M, %a, %b %d, %Y> - <summary>',
   sign_priority = 6,
   update_debounce = 100,
   status_formatter = nil, -- Use default
